0c887edc46dc5a8a75e714dddbeef3915058a812
@RupertoTacuche <murcio2490@gmail.com>
2024-10-28 20:07:26 -0600

    video_25_funcionandoo

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,53 @@
+const dotenv = require("dotenv").config()
 const express = require("express")
+const connectDB = require("./config/connectDB")
+const Task = require("./model/taskModel")
+
+
 
 const app = express()
+//Middleware
+app.use(express.json())
+//const logger = (req, res, next) => {
+//   console.log(req.method)
+//    next()
+//}
+// Routes
+app.get("/", (req, res) => {
+    res.send("Home Page")
+})
+
+// Create a task
+app.post("/api/tasks",  async (req, res) => {
+  try {
+    const task = await Task.create(req.body)
+    res.status(200).json(task)
+  } catch (error) {
+    res.status(500).json({msg: error.message})
+  }
+})   
+
+// Get/Read Data
+app.get("/api/tasks", async (req, res) => {
+  try {
+    const tasks = await Task.find()
+    res.status(200).json(tasks)
+  } catch (error) {
+    res.status(500).json({msg: error.message})
+  }
+})
 
 const PORT = process.env.PORT || 5000
 
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
-})
+const startServer = async () => {
+    try {
+        await connectDB()
+        app.listen(PORT, () => {
+        console.log(`Server running on port ${PORT}`)
+        })
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+startServer()
